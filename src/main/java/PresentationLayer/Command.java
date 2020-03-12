0bc82b979f85e2f3bba962ac3333ed6024749482
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2020-03-12 09:30:00 +0100

    Update Command.java

--- src/main/java/PresentationLayer/Command.java
+++ src/main/java/PresentationLayer/Command.java
@@ -16,11 +16,11 @@ abstract class Command {
     }
 
     static Command from( HttpServletRequest request ) {
-        String TagetName = request.getParameter( "target" );
+        String targetName = request.getParameter( "target" );
         if ( commands == null ) {
             initCommands();
         }
-        return commands.getOrDefault(TagetName, new UnknownCommand() );   // unknowncommand er default.
+        return commands.getOrDefault(targetName, new UnknownCommand() );   // unknowncommand er default.
     }
 
     abstract String execute( HttpServletRequest request, HttpServletResponse response ) 
